7659728a942db759b0e03577041cd0ea6f2d5523
Alan Boyle <aboyle@stanford.edu>
2020-06-16 16:47:49 -0400

    Update to new slack API

--- secret_santa.py
+++ secret_santa.py
@@ -4,14 +4,13 @@ import random
 import datetime
 import time
 import re
-from slackclient import SlackClient
-
-# Instantiate Slack Client
-slack_client = SlackClient("**********")
+from slack import RTMClient
+from slack.errors import SlackApiError
 
 # Assign bot user name
-BOT_NAME = None
-starterbot_id = None
+BOT_NAME = 'sclaus'
+BOT_ID = ''
+BOT_CHANNEL = ''
 
 # Family user name mapping
 userName = {
@@ -19,21 +18,38 @@ userName = {
 
 # constants
 RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
-EXAMPLE_COMMAND = "generate"
+EXAMPLE_COMMAND = "commands"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
-def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
-    for event in slack_events:
-        if event["type"] == "message" and not "subtype" in event:
-            user_id, message = parse_direct_mention(event["text"])
-            if user_id == starterbot_id:
-                return message, event["channel"], event["user"]
-    return None, None, None
+@RTMClient.run_on(event='message')
+def parse_bot_commands(**payload):
+    data = payload['data']
+    web_client = payload['web_client']
+    rtm_client = payload['rtm_client']
+
+    if 'text' in data:
+      mention, text = parse_direct_mention(data['text'])
+      if mention == BOT_ID:
+        print(mention, text)
+        handle_command(web_client, text, data['channel'], data['user'])
+
+
+
+def send_direct_message(web_client, channel_id, message_text):
+    try:
+      response = web_client.chat_postMessage(
+        channel = channel_id,
+        username="sclaus",
+        icon_emoji=":santa:",
+        text=message_text
+#        thread_ts = thread_ts
+      )
+    except SlackApiError as e:
+      assert e.response["ok"] is False
+      assert e.response["error"]
+      print(f"Got an error: {e.response['error']}")
+    return
+
 
 
 def parse_direct_mention(message_text):
@@ -46,7 +62,8 @@ def parse_direct_mention(message_text):
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 
-def handle_command(command, cmd_channel, cmd_user):
+
+def handle_command(web_client, command, cmd_channel, cmd_user):
     """
         Executes bot command if the command is known
     """
@@ -54,28 +71,24 @@ def handle_command(command, cmd_channel, cmd_user):
     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
     # Finds and executes the given command, filling in response
-    response = None
+    response = default_response
 
     # This is where you start to implement more commands!
     if command.startswith("commands"):
         response = "SClaus commands: commands, generate (generates a new list), santee (messages a reminder of your santee)";
     elif command.startswith("generate"):
-        response = generate_secret_santa()
+        response = generate_secret_santa(web_client)
     elif command.startswith("santee"):
-        response = message_santee(cmd_channel, cmd_user)
+        response = message_santee(web_client, cmd_channel, cmd_user)
     elif command.startswith("reannounce"):
-        response = reannounce_secret_santa()
+        response = reannounce_secret_santa(web_client)
 
     # Sends the response back to the channel
-    if (response):
-        slack_client.api_call(
-            "chat.postMessage",
-            channel=cmd_channel,
-            text=response or default_response
-        )
+    send_direct_message(web_client, cmd_channel, response)
 
 
-def generate_secret_santa():
+
+def generate_secret_santa(web_client):
     #get the year
     now = datetime.datetime.now()
     lastYearFile = "SS_" + str(now.year-1) + ".txt"
@@ -84,31 +97,16 @@ def generate_secret_santa():
     # Check to see if the file already exists for this year
     # indicating that the list has already been generated
     if(path.exists(thisYearFile)):
-        slack_client.api_call(
-            "chat.postMessage",
-            channel="C0HM2MJ3C",
-            username="sclaus",
-            icon_emoji=":santa:",
-            text="Secret Santa list for " + str(now.year) + " has already been generated"
-        )
+        text="Secret Santa list for " + str(now.year) + " has already been generated"
+        send_direct_message(web_client, BOT_CHANNEL, text)
         return
 
     #Announce to slack channel that we are generating a list
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="Generating the Secret Santa list for " + str(now.year) + "!!!"
-    )
+    text="Generating the Secret Santa list for " + str(now.year) + "!!!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
 
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="I am building a list that prevents you from matching with your spouse or your giftee from last year..."
-    )
+    text="I am building a list that prevents you from matching with your spouse or your giftee from last year..."
+    send_direct_message(web_client, BOT_CHANNEL, text)
 
     # Get ordered list
     f = {}
@@ -116,7 +114,7 @@ def generate_secret_santa():
         family = line.strip().split(" ")
         f.update({p: i for p in family})
 
-    santas = f.keys()
+    santas = list(f.keys())
 
     #prevent same match as last year
     ly = {}
@@ -127,41 +125,36 @@ def generate_secret_santa():
     while True:
         random.shuffle(santas)
         assignments = {a: b for a,b in zip(santas, santas[1:] + [santas[0]])}
-        if all([f[a] != f[b] for a,b in assignments.iteritems()]):
-            if all([b != ly[a] for a,b in assignments.iteritems()]):
+        if all([f[a] != f[b] for a,b in assignments.items()]):
+            if all([b != ly[a] for a,b in assignments.items()]):
                 break
+
 
     #save this for next year
     out = open(thisYearFile, "w")
-    for a, b in assignments.iteritems():
+    for a, b in assignments.items():
         out.write("{} {}\n".format(a, b))
+    out.close()
 
-    #Now talk to slack
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            send_user_santee(user.get('id'), thisYearFile)
-
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="DONE! Please check your private messages from me to get your new Santee!"
-    )
-
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="If you don't get a message, please let me know here!"
-    )
+    announce_all(web_client, thisYearFile)
+    text="DONE! Please check your private messages from me to get your new Santee!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
+    text="If you don't get a message, please let me know here!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
 
 
-def reannounce_secret_santa():
+
+def announce_all(web_client, thisYearFile):
+    api_call = web_client.users_list()
+    users = api_call["members"]
+    user_names = list(map(lambda u: u["name"], users))
+    user_map = {user["name"]:user["id"] for user in users}
+    for user in user_names:
+        send_user_santee(web_client, user_map[user], thisYearFile)
+    return
+
+
+def reannounce_secret_santa(web_client):
     #get the year
     now = datetime.datetime.now()
     thisYearFile = "SS_" + str(now.year) + ".txt"
@@ -169,121 +162,70 @@ def reannounce_secret_santa():
     # Check to see if the file already exists for this year
     # indicating that the list has already been generated
     if(not path.exists(thisYearFile)):
-        slack_client.api_call(
-            "chat.postMessage",
-            channel="C0HM2MJ3C",
-            username="sclaus",
-            icon_emoji=":santa:",
-            text="Secret Santa list for " + str(now.year) + " has not yet been generated"
-        )
+        text="Secret Santa list for " + str(now.year) + " has not yet been generated"
+        send_direct_message(web_client, BOT_CHANNEL, text)
         return
 
     #Announce to slack channel that we are generating a list
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="Re-announcing the Secret Santa list for " + str(now.year) + "!!!"
-    )
+    text="Re-announcing the Secret Santa list for " + str(now.year) + "!!!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
+    text="At any point you can send the command @sclaus santee to have me pm you your match."
+    send_direct_message(web_client, BOT_CHANNEL, text)
 
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="At any point you can send the command @sclaus santee to have me pm you your match."
-    )
-
-    #Now talk to slack
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            send_user_santee(user.get('id'), thisYearFile)
-
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="DONE! Please check your private messages from me to get your new Santee!"
-    )
-
-    slack_client.api_call(
-        "chat.postMessage",
-        channel="C0HM2MJ3C",
-        username="sclaus",
-        icon_emoji=":santa:",
-        text="If you don't get a message, please let me know here!"
-    )
+    announce_all(web_client, thisYearFile)
+    text="DONE! Please check your private messages from me to get your new Santee!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
+    text="If you don't get a message, please let me know here!"
+    send_direct_message(web_client, BOT_CHANNEL, text)
 
 
 
-def message_santee(cmd_channel, cmd_user):
+def message_santee(web_client, cmd_channel, cmd_user):
     #get the year
     now = datetime.datetime.now()
     thisYearFile = "SS_" + str(now.year) + ".txt"
     # Check to see if the file already exists for this year
     # indicating that the list has already been generated
     if(not path.exists(thisYearFile)):
-        slack_client.api_call(
-            "chat.postMessage",
-            channel=cmd_channel,
-            username="sclaus",
-            icon_emoji=":santa:",
-            text="Secret Santa list for " + str(now.year) + " has not yet been generated"
-        )
+        text="Secret Santa list for " + str(now.year) + " has not yet been generated"
+        send_direct_message(web_client, cmd_channel, text)
         return
 
-    return send_user_santee(cmd_user, thisYearFile)
+    return send_user_santee(web_client, cmd_user, thisYearFile)
 
 
-def send_user_santee(cmd_user, file):
+
+def send_user_santee(web_client, cmd_user, file):
     assignments = {}
     with open(file) as f:
         for line in f:
             (key, val) = line.split()
             assignments[userName[key]] = val
+    f.close()
 
-    print("Request: ", cmd_user)
+    print("Request:", cmd_user)
 
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-#            print(user.get('name'), user.get('id'))
-            if 'name' in user and user.get('id') == cmd_user and user.get('name') in assignments:
-                print("Bot ID for '" + user.get('name') + "' is " + user.get('id'))
-                print("Selection for " + user.get('name') + " is " + assignments[user.get('name')])
+    api_call = web_client.users_list()
+    users = api_call["members"]
+    user_map = {user["id"]:user["name"] for user in users}
 
-                giftee = assignments[user.get('name')]
+    # retrieve all users so we can find our bot
+    user = user_map[cmd_user]
+    if user in assignments.keys():
+        print("Bot ID for '" + user + "' is " + cmd_user)
+        print("Selection for " + user + " is " + assignments[user])
 
-                slack_client.api_call(
-                    "chat.postMessage",
-                    channel=user.get('id'),
-                    username="sclaus",
-                    icon_emoji=":santa:",
-                    as_user="U2U1F7M8C",
-                    text="This year you will be getting a gift for: " + giftee
-                )
+        giftee = assignments[user]
 
-    else:
-        print("could not find bot user with the name " + cmd_user)
+        text="This year you will be getting a gift for: " + giftee
+        response = web_client.conversations_open(users=[cmd_user])
+        conversation_id = response['channel']['id']
+        send_direct_message(web_client, conversation_id, text)
+    time.sleep(5)
+    return
 
 
-# Main function
-if __name__ == "__main__":
-    if slack_client.rtm_connect():
-        print("Secret Santa connected and running!")
-        # Read bot's user ID by calling Web API method `auth.test`
-        starterbot_id = slack_client.api_call("auth.test")["user_id"]
-        while True:
-            command, channel, user = parse_bot_commands(slack_client.rtm_read())
-            if command:
-                handle_command(command, channel, user)
-            time.sleep(RTM_READ_DELAY)
-    else:
-        print("Connection failed. Exception traceback printed above.")
+
+# Instantiate Slack Client
+rtm_client = RTMClient(token="******************")
+rtm_client.start()
